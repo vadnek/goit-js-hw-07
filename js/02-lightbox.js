aed634148482eb276dd17d754b81888b08864042
Vadim Lukyanenko <vadneek@yahoo.com>
2022-11-13 21:38:28 +0200

    Final version

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const createGalleryListMarkup = gallery =>
+  gallery
+    .map(
+      ({ description, original, preview }) => 
+      `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`
+    )
+    .join('');
+
+    const galleryItemsList = createGalleryListMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsList);
+
+const lightbox = new SimpleLightbox('.gallery a', 
+{captionsData: 'alt',
+captionPosition: 'bottom', // По умолчание тоже bottom:)
+captionDelay: 250});
+
